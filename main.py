6f6b928ef27442cf076cae937482a41e7a19760d
eva1617 <105513556+eva1617@users.noreply.github.com>
2024-07-07 14:17:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -196,54 +196,35 @@ def get_sorted_scores(firebase_url, path):
         return sorted_score_list
     else:
         return []
-
-from linebot import LineBotApi
-from linebot.exceptions import LineBotApiError
-
-# 初始化LineBotApi
-line_bot_api = LineBotApi(channel_access_token)
-def get_user_profile(user_id):
-    try:
-        # 使用LineBotApi的get_profile方法來獲取使用者資料
-        profile = line_bot_api.get_profile(user_id)
-        return profile.display_name  # 返回使用者的顯示名稱
-    except LineBotApiError as e:
-        print("LineBotApiError:", e)
-        return None
-
-
+        
 def get_rank(current_user_id,firebase_url):
 
     # 設定表格的欄位寬度
-    rank_width = 7
-    user_width = 14
-    score_width = 11
+    rank_width = 8
+    user_width = 15
+    score_width = 12
     total_width = rank_width + user_width + score_width + 4  # 包括分隔符號
 
-    sorted_scores = get_sorted_scores(firebase_url, 'scores/')
+    sorted_scores = get_sorted_scores(firebase_url,'scores/')
 
     # 初始化表格字串
     table_str = ''
 
     # 表格頂部邊界
     table_str += '+' + '-' * total_width + '+\n'
-    table_str += '|' + "排行榜".center(total_width-3) + '|\n'
+    table_str += '|' + "排行榜".center(total_width) + '|\n'
     table_str += '+' + '-' * total_width + '+\n'
     table_str += f"|{'排名'.center(rank_width)}|{'User'.center(user_width)}|{'Score'.center(score_width)}|\n"
     table_str += '+' + '-' * rank_width + '+' + '-' * user_width + '+' + '-' * score_width + '+\n'
 
     if sorted_scores:
         i = 1
-        for user_id, score in sorted_scores:
-            user_name = get_user_profile(user_id, channel_access_token)
-            if user_name is None:
-                user_display = user_id[:5]  # 如果無法取得使用者名稱，顯示部分使用者 ID
-            else:
-                user_display = user_name[:user_width]
-
+        for user, score in sorted_scores:
             # 標記當前使用者
-            if user_id == current_user_id:
-                user_display = f'*{user_display}*'
+            if user == current_user_id:
+                user_display = f'Your ID'
+            else:
+                user_display = user[:5]
 
             table_str += f"|{str(i).center(rank_width)}|{user_display.center(user_width)}|{str(score).center(score_width)}|\n"
             table_str += '+' + '-' * rank_width + '+' + '-' * user_width + '+' + '-' * score_width + '+\n'
@@ -252,6 +233,7 @@ def get_rank(current_user_id,firebase_url):
         table_str += '|' + '目前無人上榜'.center(total_width) + '|\n'
         table_str += '+' + '-' * total_width + '+\n'
     return table_str
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
